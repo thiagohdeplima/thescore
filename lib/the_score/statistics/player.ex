f35ca3b27775ea96a32ca7ae214fddc7aa0c7905
Thiago Henrique de Paulo Lima <thiago.henrique@semantix.com.br>
2021-06-01 00:23:54 -0300

    chore(player): added fields on ecto schema

--- lib/the_score/statistics/player.ex
+++ lib/the_score/statistics/player.ex
@@ -5,6 +5,24 @@ defmodule TheScore.Statistics.Player do
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
   schema "players" do
+    field :name, :string
+    field :team, :string
+    field :pos,  :string
+
+    field :attg,  :float
+    field :att,   :float
+    field :yds,   :float
+    field :avg,   :float
+    field :ydsg,  :float
+    field :td,    :float
+    field :lng,   :string
+    field :r1st,  :float
+    field :r1stp, :float
+    
+    field :r20plus, :float
+    field :r40plus, :float
+
+    field :fum, :float
 
     timestamps()
   end
@@ -12,7 +30,14 @@ defmodule TheScore.Statistics.Player do
   @doc false
   def changeset(player, attrs) do
     player
-    |> cast(attrs, [])
-    |> validate_required([])
+    |> cast(attrs, fields())
+    |> validate_required(fields())
+  end
+
+  defp fields do
+    __MODULE__.__schema__(:fields)
+    |> Enum.filter(fn field ->
+      not Enum.member?([:id, :inserted_at, :updated_at], field)
+    end)
   end
 end
