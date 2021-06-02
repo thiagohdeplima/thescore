606235e3fa144285e292cc4c52c2e7998cacbacf
Thiago Henrique de Paulo Lima <thiago.henrique@semantix.com.br>
2021-06-01 21:01:07 -0300

    chore: grouped test and dev deps

--- mix.exs
+++ mix.exs
@@ -38,15 +38,17 @@ defmodule TheScore.MixProject do
       {:ecto_sql, "~> 3.4"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_live_view, "~> 0.15.1"},
-      {:floki, ">= 0.30.0", only: :test},
       {:phoenix_html, "~> 2.11"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_dashboard, "~> 0.4"},
       {:telemetry_metrics, "~> 0.4"},
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+
+      # Test and Dev dependencies
+      {:floki, ">= 0.30.0", only: :test},
+      {:phoenix_live_reload, "~> 1.2", only: :dev},
     ]
   end
 
