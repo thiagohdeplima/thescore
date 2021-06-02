410049fe22dfed441b5ca33d15f7fb255ac9c2af
Thiago Henrique de Paulo Lima <thiago.henrique@semantix.com.br>
2021-06-01 22:02:47 -0300

    chore: warnings as errors

--- mix.exs
+++ mix.exs
@@ -10,7 +10,10 @@ defmodule TheScore.MixProject do
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      elixirc_options: [
+        warnings_as_errors: true
+      ]
     ]
   end
 
