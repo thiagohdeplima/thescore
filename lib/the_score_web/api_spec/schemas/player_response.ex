81ec094ee298578734e6146846f70fdd9fc2f277
Thiago Henrique de Paulo Lima <thiago.henrique@semantix.com.br>
2021-06-03 14:45:01 -0300

    fix: solved type breach in api spec

--- lib/the_score_web/api_spec/schemas/player_response.ex
+++ lib/the_score_web/api_spec/schemas/player_response.ex
@@ -11,7 +11,8 @@ defmodule TheScore.ApiSpec.Schemas.PlayerResponse do
     type: :object,
     properties: %{
       id: %Schema{
-        type: :uuid
+        type: :string,
+        format: :uuid
       },
       name: %Schema{
         type: :string,
@@ -26,52 +27,63 @@ defmodule TheScore.ApiSpec.Schemas.PlayerResponse do
         description: "Player's postion"
       },
       attg: %Schema{
-        type: :float,
+        type: :number,
+        format: :float,
         description: "Rushing Attempts Per Game Average"
       },
       att: %Schema{
-        type: :float,
+        type: :number,
+        format: :float,
         description: "Rushing Attempts"
       },
       yds: %Schema{
-        type: :float,
+        type: :number,
+        format: :float,
         description: "Total Rushing Yards"
       },
       avg: %Schema{
-        type: :float,
+        type: :number,
+        format: :float,
         description: "Rushing Average Yards Per Attempt"
       },
       ydsg: %Schema{
-        type: :float,
+        type: :number,
+        format: :float,
         description: "Rushing Yards Per Game"
       },
       td: %Schema{
-        type: :float,
+        type: :number,
+        format: :float,
         description: "Total Rushing Touchdowns"
       },
       lng: %Schema{
         type: :string,
         description: "Longest Rush -- a T represents a touchdown occurred",
-        pattern: ~r/(\d+){1,}.(\d+){1,}(T)?/
+        pattern: ~r/(\d+){1,}(.(\d+){1,})?(T)?/
       },
       r1st: %Schema{
-        type: :float,
+        type: :number,
+        format: :float,
         description: "Rushing First Downs"
       },
       r1stp: %Schema{
-        type: :float,
+        type: :number,
+        format: :float,
         description: "Rushing First Down Percentage"
       },
       r20plus: %Schema{
-        type: :float,
+        type: :number,
+        format: :float,
         description: "Rushing 20+ Yards Each"
       },
       r40plus: %Schema{
-        type: :float,
+        type: :number,
+        format: :float,
         description: "Rushing 40+ Yards Each"
       },
       fum: %Schema{
-        type: :float,
+        type: :number,
+        format: :float,
         description: "Rushing Fumbl"
       }
     },
